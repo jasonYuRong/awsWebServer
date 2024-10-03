070cd836c8bcdbc9dd4db2be71904441cdf7f680
jasonYuRong <jason122130@gmail.com>
2024-10-02 23:49:41 -0400

    my first aws web server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import { searchProduct, placeOrder } from './snsService';
 
 function App() {
+  
+  const [productID, setProductID] = useState('');
+  const [productName, setProductName] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const handleSearchProduct = () => {
+    console.log("Search product clicked");
+    searchProduct(productID, productName);
+  };
+
+  const handlePlaceOrder = () => {
+    console.log("Place order clicked");
+    placeOrder(productID, quantity);
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          e-commerce
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <h2>Search Product</h2>
+        <input
+          type="text"
+          placeholder="Product ID"
+          value={productID}
+          onChange={(e) => setProductID(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+        />
+        <button onClick={handleSearchProduct}>Search Product</button>
+      </div>
+      <div>
+        <h2>Place Order</h2>
+        <input
+          type="text"
+          placeholder="Product ID"
+          value={productID}
+          onChange={(e) => setProductID(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <button onClick={handlePlaceOrder}>Place Order</button>
+      </div>
     </div>
   );
 }
